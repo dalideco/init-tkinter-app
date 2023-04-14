6d8d6bd3c0087218c59e0e19d7bb62c3bdef41cf
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2023-04-14 03:32:48 +0100

    fixing the output

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 import tkinter as tk
-import subprocess
 import os
 
 class CustomTkinterGUI:
     def __init__(self, master):
         self.master = master
-        master.title("CustomTkinter GUI")
+        master.title("code")
+
+        
+        # create input frame
+        input_frame = tk.Frame(master)
+        input_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # create line number label for input text area
+        self.line_numbers = tk.Label(input_frame, width=5, justify=tk.RIGHT, anchor='nw')
+        self.line_numbers.pack(side=tk.LEFT, fill=tk.Y)
 
         # create input text area
-        self.input_text = tk.Text(master, height=10, width=50)
-        self.input_text.pack()
+        self.input_text = tk.Text(input_frame, height=30, width=80)
+        self.input_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.input_text.bind('<Key>', self.update_linenumbers)
+
+        # create scrollbar for input text area
+        scrollbar = tk.Scrollbar(input_frame)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollbar.config(command=self.input_text.yview)
+        self.input_text.config(yscrollcommand=scrollbar.set)
 
         # create "Run" button
-        self.run_button = tk.Button(master, text="Run", command=self.run_code)
+        self.run_button = tk.Button(master, text="▶", command=self.run_code , background="green")
         self.run_button.pack()
+
+    def update_linenumbers(self, event):
+        line_numbers = ''
+        input_lines = self.input_text.get('1.0', 'end').split('\n')
+        for i in range(1, len(input_lines)):
+            line_numbers += str(i) + '\n'
+        self.line_numbers.config(text=line_numbers)
+
 
     def run_code(self):
         # get the input code from the input text area
@@ -25,17 +48,24 @@ class CustomTkinterGUI:
 
         # run the compiled binary file with the text file as input
         # ./miniCompiler<input.txt
-        output = subprocess.Popen(["./miniCompiler", "<input.txt"], stdout=subprocess.PIPE).communicate()
-        print("entire output: "+ output )
-        output= output[0]
+        os.system("./miniCompiler < input.txt > output.txt 2>&1 ")
 
-        print("ouput variable: "+ output)
-      
+        # read the output from the output file
+        with open("output.txt", "r") as f:
+            output = f.read()
+
         # create a new window to display the output
         output_window = tk.Toplevel(self.master)
         output_window.title("Output")
-        output_text = tk.Text(output_window, height=10, width=50)
-        output_text.pack()
+        output_text = tk.Text(output_window, height=30, width=80)
+        output_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # create scrollbar for output text area
+        scrollbar = tk.Scrollbar(output_window)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollbar.config(command=output_text.yview)
+        output_text.config(yscrollcommand=scrollbar.set)
+
         output_text.insert(tk.END, output)
 
 if __name__ == '__main__':
--- miniCompiler.tab.h
+++ miniCompiler.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.5.1.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,105 +31,77 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_YY_MINICOMPILER_TAB_H_INCLUDED
+# define YY_YY_MINICOMPILER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     Ident = 258,
-     int_type = 259,
-     boolean_type = 260,
-     string_type = 261,
-     Boolean_Literal = 262,
-     Integer_Literal = 263,
-     String_Literal = 264,
-     class_keyword = 265,
-     public_keyword = 266,
-     main_class_keyword = 267,
-     extends_keyword = 268,
-     return_keyword = 269,
-     if_keyword = 270,
-     else_keyword = 271,
-     whileloop_keyword = 272,
-     print_keyword = 273,
-     length_keyword = 274,
-     this_keyword = 275,
-     new_keyword = 276,
-     open_parenthese = 277,
-     closed_parenthese = 278,
-     open_bracket = 279,
-     closed_bracket = 280,
-     open_brace = 281,
-     closed_brace = 282,
-     OP_AFFECT = 283,
-     OP_ADD = 284,
-     OP_SUBSTRACT = 285,
-     OP_MULTIPLY = 286,
-     OP_NOT = 287,
-     LOG_AND = 288,
-     LOG_LESS = 289,
-     LOG_EQLESS = 290,
-     LOG_MORE = 291,
-     LOG_EQMORE = 292,
-     LOG_EQ = 293,
-     LOG_DIF = 294,
-     SEMI_COLON = 295,
-     DOT = 296,
-     COMMA = 297
-   };
+  enum yytokentype
+  {
+    Ident = 258,
+    int_type = 259,
+    boolean_type = 260,
+    string_type = 261,
+    Boolean_Literal = 262,
+    Integer_Literal = 263,
+    String_Literal = 264,
+    class_keyword = 265,
+    public_keyword = 266,
+    main_class_keyword = 267,
+    extends_keyword = 268,
+    return_keyword = 269,
+    if_keyword = 270,
+    else_keyword = 271,
+    whileloop_keyword = 272,
+    print_keyword = 273,
+    length_keyword = 274,
+    this_keyword = 275,
+    new_keyword = 276,
+    open_parenthese = 277,
+    closed_parenthese = 278,
+    open_bracket = 279,
+    closed_bracket = 280,
+    open_brace = 281,
+    closed_brace = 282,
+    OP_AFFECT = 283,
+    OP_ADD = 284,
+    OP_SUBSTRACT = 285,
+    OP_MULTIPLY = 286,
+    OP_NOT = 287,
+    LOG_AND = 288,
+    LOG_LESS = 289,
+    LOG_EQLESS = 290,
+    LOG_MORE = 291,
+    LOG_EQMORE = 292,
+    LOG_EQ = 293,
+    LOG_DIF = 294,
+    SEMI_COLON = 295,
+    DOT = 296,
+    COMMA = 297
+  };
 #endif
-/* Tokens.  */
-#define Ident 258
-#define int_type 259
-#define boolean_type 260
-#define string_type 261
-#define Boolean_Literal 262
-#define Integer_Literal 263
-#define String_Literal 264
-#define class_keyword 265
-#define public_keyword 266
-#define main_class_keyword 267
-#define extends_keyword 268
-#define return_keyword 269
-#define if_keyword 270
-#define else_keyword 271
-#define whileloop_keyword 272
-#define print_keyword 273
-#define length_keyword 274
-#define this_keyword 275
-#define new_keyword 276
-#define open_parenthese 277
-#define closed_parenthese 278
-#define open_bracket 279
-#define closed_bracket 280
-#define open_brace 281
-#define closed_brace 282
-#define OP_AFFECT 283
-#define OP_ADD 284
-#define OP_SUBSTRACT 285
-#define OP_MULTIPLY 286
-#define OP_NOT 287
-#define LOG_AND 288
-#define LOG_LESS 289
-#define LOG_EQLESS 290
-#define LOG_MORE 291
-#define LOG_EQMORE 292
-#define LOG_EQ 293
-#define LOG_DIF 294
-#define SEMI_COLON 295
-#define DOT 296
-#define COMMA 297
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef int YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
+int yyparse (void);
+
+#endif /* !YY_YY_MINICOMPILER_TAB_H_INCLUDED  */
